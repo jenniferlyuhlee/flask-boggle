c3e64207ffb7a03f6052ccbefd04694be1a07559
jenniferlyuhlee <jenniferlyuhlee@gmail.com>
2024-04-01 19:47:06 -0700

    completed draft before class refactoring

--- app.py
+++ app.py
@@ -20,11 +20,29 @@ def show_board():
                            num_plays = num_plays)
 
 
-@app.route('/check_word')
+@app.route('/check-word')
 def check_word():
     """Checks if guessed word is in dictionary"""
+
     board = session['board']
     guess = request.args['guess']
     result = boggle_game.check_valid_word(board, guess)
 
     return jsonify({"result": result})
+
+
+@app.route('/end-game', methods=["POST"])
+def end_game():
+    """Handles end of game, updating high score and number of plays"""
+
+    score = request.json['score']
+    highscore = session.get('highscore', 0)
+    num_plays = session.get('num_plays', 0)
+
+    session['highscore'] = max(score, highscore)
+    session['num_plays'] = num_plays + 1
+
+    #returns true if highscore was broken
+    new_highscore = (score > highscore)
+    return jsonify({"new_highscore": new_highscore})
+
--- static/app.js
+++ static/app.js
@@ -1,9 +1,12 @@
 let score = 0;
-let sec = 60;
+let sec = 5;
+let correct_guesses = new Set();
 
-
-//timer resource:
-//https://www.youtube.com/watch?time_continue=348&v=x7WJEmxNlEs&embeds_referring_euri=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dhow%2Bto%2Bput%2Ba%2Btimer%2Bon%2Ba%2Bgame%2Bjs%2Bdom%2Bjs%26sca_esv%3D97d3c4185a5a42ee%26rlz%3D1C5CHFA_enUS939US939%26sxsrf%3DA&source_ve_path=Mjg2NjMsMjg2NjMsMjM4NTE&feature=emb_title
+/** Sets timer and stops guesses when timer is up
+ * 
+ * timer resource:
+ * https://www.youtube.com/watch?time_continue=348&v=x7WJEmxNlEs&embeds_referring_euri=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dhow%2Bto%2Bput%2Ba%2Btimer%2Bon%2Ba%2Bgame%2Bjs%2Bdom%2Bjs%26sca_esv%3D97d3c4185a5a42ee%26rlz%3D1C5CHFA_enUS939US939%26sxsrf%3DA&source_ve_path=Mjg2NjMsMjg2NjMsMjM4NTE&feature=emb_title
+ */ 
 const timer = setInterval(countdown, 1000);
 
 function countdown(){
@@ -11,44 +14,67 @@ function countdown(){
     sec--; 
     if (sec < 0){
         clearInterval(timer);
+        endGame();
     }
 }
 
+/**
+ * Front-end display functions
+ */
 function displayTimer(sec){
     $(".timer").text(`${sec}`)
 }
 
-
-function displayMessage(msg){
-    $(".response").html(msg)
+function displayMessage(msg, cls){
+    $("#response").removeClass().addClass(`${cls}`).html(msg);
 }
-
 
 function displayScore(score){
     $(".score").text(score)
 }
 
-
+/**
+ * handles guess by checking response from server and displaying corresponding message
+ */
 async function handleGuess(evt){
     evt.preventDefault();
     let guess = $("#guess").val();
-    const resp = await axios.get("/check-word", {params: {guess}});
+    console.log(guess);
+    const resp = await axios.get('/check-word', {params: {guess}});
     
     //checks for word validity from server response
     if (resp.data.result === 'not-on-board'){
-        displayMessage(`<bold>${guess}</bold> isn't on the board`);
+        displayMessage(`<b>${guess.toUpperCase()}</b> isn't on the board!`, 'invalid');
     }
-    else if (resp.data.result === 'not-a-word'){
-        displayMessage(`<bold>${guess}</bold> isn't a word`);
+    else if (resp.data.result === 'not-word'){
+        displayMessage(`<b>${guess.toUpperCase()}</b> isn't a valid word!`, 'invalid');
     }
-    else{
-        displayMessage(`<bold>${guess}</bold> is on the board!`);
-        score += guess.length;
+    else if (resp.data.result === 'ok'){
+        if (correct_guesses.has(guess)){
+            displayMessage(`You already guessed <b>${guess.toUpperCase()}</b>!`, 'invalid');
+        }
+        else{
+            displayMessage(`<b>${guess.toUpperCase()}</b> is on the board!`, 'valid');
+            correct_guesses.add(guess);
+            score += guess.length;
+        }
         displayScore(score);
     }
+    $("#submit-word-form").trigger('reset');
 }
-$("#submit-word-form").on("submit", handleGuess)
+$("#submit-word-form").on("submit", handleGuess);
 
-// async function endGame(){
 
-// }
+/** Stop guessing and checks if score beat highscore by sending score to server
+ *  Displays appropriate message
+ */
+async function endGame(){
+    $('#submit-button').prop('disabled', true);
+    const resp = await axios.post('/end-game', {score: score});
+    if (resp.data.new_highscore){
+        displayMessage(`Hooray! You set a new highscore: <b>${score}<b>` , 'finish');
+    }
+    else{
+        displayMessage(`Good job, you scored: <b>${score}<b>`, 'finish');
+    }
+}
